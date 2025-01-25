db7f258f975439a8bb7df69a4aec01ab1f37062b
Scriptor25 <f.schreiber.2006@proton.me>
2025-01-25 23:41:14 +0100

    catching null case in some classes

--- src/main/java/io/scriptor/instruction/SetAttribInstruction.java
+++ src/main/java/io/scriptor/instruction/SetAttribInstruction.java
@@ -1,18 +1,18 @@
 /*
  * This file is part of https://github.com/Scriptor25/LogicSim
- * 
+ *
  * Copyright (C) 2025  Felix Schreiber
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see https://www.gnu.org/licenses/.
  */
@@ -21,6 +21,7 @@ package io.scriptor.instruction;
 import io.scriptor.context.State;
 import io.scriptor.function.Function;
 import io.scriptor.util.IOStream;
+import io.scriptor.util.RTException;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
@@ -38,7 +39,10 @@ public record SetAttribInstruction(
         final var uuid = IOStream.readUUID(inputStream);
         final var attrib = IOStream.readUUID(inputStream);
         final var value = IOStream.readUUID(inputStream);
-        function.add(new SetAttribInstruction(uuid, attrib, function.find(value)));
+        final var valueInstruction = function.find(value);
+        if (valueInstruction == null)
+            throw new RTException("invalid value instruction id %s", value);
+        function.add(new SetAttribInstruction(uuid, attrib, valueInstruction));
     }
 
     public SetAttribInstruction(final @NotNull UUID attrib, final @NotNull Instruction value) {
--- src/main/java/io/scriptor/instruction/SetRegInstruction.java
+++ src/main/java/io/scriptor/instruction/SetRegInstruction.java
@@ -1,18 +1,18 @@
 /*
  * This file is part of https://github.com/Scriptor25/LogicSim
- * 
+ *
  * Copyright (C) 2025  Felix Schreiber
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see https://www.gnu.org/licenses/.
  */
@@ -21,6 +21,7 @@ package io.scriptor.instruction;
 import io.scriptor.context.State;
 import io.scriptor.function.Function;
 import io.scriptor.util.IOStream;
+import io.scriptor.util.RTException;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
@@ -40,7 +41,10 @@ public record SetRegInstruction(
         final var reg = IOStream.readUUID(inputStream);
         final var index = IOStream.readInt(inputStream);
         final var value = IOStream.readUUID(inputStream);
-        function.add(new SetRegInstruction(uuid, reg, index, function.find(value)));
+        final var valueInstruction = function.find(value);
+        if (valueInstruction == null)
+            throw new RTException("invalid value instruction id %s", value);
+        function.add(new SetRegInstruction(uuid, reg, index, valueInstruction));
     }
 
     public SetRegInstruction(final @NotNull UUID reg, final int index, final @NotNull Instruction value) {
